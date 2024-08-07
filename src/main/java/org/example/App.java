653e3868adf430e1065eeee963fbd38c85a417e4
karim-ledoyen <karim.ledoyen@gmail.com>
2024-08-07 19:56:33 +0300

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+/**
+ * git : a version control system using it i will save version of code and reduce conflicts btwn team members
+ * i need to install git local , create an account of any git reporsatory website
+ * github , gitlab , azure devops
+ * a new memebr joined our team so we have out repo
+ * 1- clone the repo and start to iintegrate with our  framwork on his branch and then open pr abroved then merge it to master
+ * 2- i am trying to implement a new framework into new repo
+ * init my poject as agit projetc
+ * git init
+ * add all fils i need to upload git add + file namw
+ * git commit -m "first commit"
+ * git push repo url
+ */
